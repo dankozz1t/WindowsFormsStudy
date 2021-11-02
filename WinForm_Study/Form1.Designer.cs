8abc91a42340abd94c8f2615e87813ba98fdf0e8
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-02 13:47:07 +0200

    StartWindowsForms

---
+++ WinForm_Study/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace WinForm_Study
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GlobalBtm = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GlobalBtm
+            // 
+            this.GlobalBtm.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.GlobalBtm.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.GlobalBtm.FlatAppearance.BorderColor = System.Drawing.Color.OliveDrab;
+            this.GlobalBtm.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.GlobalBtm.ForeColor = System.Drawing.Color.Gold;
+            this.GlobalBtm.Location = new System.Drawing.Point(360, 421);
+            this.GlobalBtm.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.GlobalBtm.Name = "GlobalBtm";
+            this.GlobalBtm.Size = new System.Drawing.Size(244, 63);
+            this.GlobalBtm.TabIndex = 0;
+            this.GlobalBtm.Text = "Кнопка";
+            this.GlobalBtm.UseVisualStyleBackColor = false;
+            this.GlobalBtm.Click += new System.EventHandler(this.GlobalBtm_Click);
+            this.GlobalBtm.MouseClick += new System.Windows.Forms.MouseEventHandler(this.GlobalBtm_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Teal;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(77, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "НАЖМИ";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Montserrat Black", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Orange;
+            this.label2.Location = new System.Drawing.Point(428, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "12:12:12";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(360, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 248);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(922, 521);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GlobalBtm);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Montserrat", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Мой первая формочка";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GlobalBtm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WinForm_Study/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_Study
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GlobalBtm_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Аууууууууууууу!!!", "Ау", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation);
+            if (res == DialogResult.Yes)
+            {
+                MessageBox.Show("Вы нажали ДА", "Сработало нажатие ДА", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.GlobalBtm.Text = $"{e.X.ToString()} | {e.Y.ToString()}";
+
+            if ((e.X > label1.Location.X - 20 && e.X < label1.Location.X + label1.Width + 20) &&
+                (e.Y > label1.Location.Y - 20 && e.Y < label1.Location.Y + label1.Height + 20))
+            {
+                if (e.X > label1.Location.X - 20 && e.X < label1.Location.X) //движение курсора с лева по оси Х
+                {
+                    label1.Left += 10;
+                }
+                else if (e.X < label1.Location.X + label1.Width + 20 &&
+                         e.X > label1.Location.X + label1.Width) //движение курсора с права по оси Х
+                {
+                    label1.Left -= 10;
+                }
+                else if (e.Y > label1.Location.Y - 20 && e.Y < label1.Location.Y) //движение курсора с верху по оси У
+                {
+                    label1.Top += 10;
+                }
+                else if (e.Y < label1.Location.Y + label1.Height + 20 &&
+                         e.Y > label1.Location.Y + label1.Height) //движение курсора с низу по оси У
+                {
+                    label1.Top -= 10;
+                }
+
+                //Проверка границ окна и возврат «статика» в центр
+                if ((label1.Location.X < 0 || label1.Location.X > ClientSize.Width - label1.Width) ||
+                    (label1.Location.Y < 0 || label1.Location.Y > ClientSize.Height - label1.Height))
+                {
+                    LableCenter(label1);
+                }
+
+            }
+        }
+
+        void LableCenter(Label lable) //центрирование «статика»
+        {
+            label1.Left = (ClientSize.Width - label1.Size.Width) / 2;
+            label1.Top = (ClientSize.Height - label1.Size.Height) / 2;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string txt = "Вы нажали ";
+            if (e.Button == MouseButtons.Left)
+            {
+                txt += "ЛЕВУЮ";
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                txt += "ПРАВУЮ";
+            }
+
+            if (e.Button == MouseButtons.Middle)
+            {
+                txt += "ЦЕНТРАЛЬНУЮ(КОЛЁСИКО)";
+            }
+            txt += " кнопку миши";
+
+            MessageBox.Show(txt, "Нажатие миши", MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+
+        private void GlobalBtm_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MessageBox.Show("Правая Кнопка миши", "Нажатие миши", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ЗАЙКА!", "вы нажали на зайку", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
