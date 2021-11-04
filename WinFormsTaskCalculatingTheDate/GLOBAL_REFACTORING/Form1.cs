c2df804e17ee8e914ee5a3a3c582a3996f6b4b07
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-04 15:30:11 +0200

    Global refactoring from Nerf

---
+++ WinFormsTaskCalculatingTheDate/GLOBAL_REFACTORING/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsTaskCalculatingTheDate
+{
+    public partial class Form1 : Form
+    {
+        private DateTime date;
+        private DateTime dateTimeNow = DateTime.Now;
+
+        private string incorrectInputName = "Некоректный ввод";
+        private string timeLessTodayName = "Время меньше сегодняшнего!!";
+        private string errorName = "Ошибка!";
+
+        public Form1() => InitializeComponent();
+        private void sendDataButton_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = "";
+
+            if (radioButtonTime.Checked)
+            {
+                DateApply(textBoxTime, false);
+            }
+            else
+            {
+                DateApply(textBoxDate, true);
+            }
+        }
+        private void radioButtonTime_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButtonChange(radioButtonTime, textBoxTime);
+        }
+
+        private void radioButtonDate_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButtonChange(radioButtonDate, textBoxDate);
+        }
+
+        private void RadioButtonChange(RadioButton radioButton, MaskedTextBox textBox)
+        {
+            if (radioButton.Checked)
+            {
+                textBox.Enabled = true;
+            }
+            else
+            {
+                textBox.Enabled = false;
+            }
+        }
+
+        private void textBoxDate_TypeValidationCompleted(object sender, TypeValidationEventArgs e)
+        {
+            if (!e.IsValidInput)
+            {
+                SendError(textBoxDate, incorrectInputName);
+            }
+        }
+
+        private void textBoxTime_TypeValidationCompleted(object sender, TypeValidationEventArgs e)
+        {
+            if (!e.IsValidInput)
+            {
+                SendError(textBoxTime, incorrectInputName);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelNow.Text = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString();
+        }
+
+        private void DateApply(MaskedTextBox maskedTextBox, bool isTimeSizeBig)
+        {
+            if (maskedTextBox.MaskCompleted)
+            {
+                date = DateTime.Parse(maskedTextBox.Text);
+                if (date > dateTimeNow)
+                {
+                    SetTextBox(isTimeSizeBig);
+                }
+                else
+                {
+                    //Придумать логику проработки времени 00-23
+                    SendError(maskedTextBox, timeLessTodayName);
+                }
+            }
+            else
+            {
+                SendError(maskedTextBox, incorrectInputName);
+            }
+        }
+
+        private void SetTextBox(bool isTimeSizeBig)
+        {
+            TimeSpan timeSpan = date - dateTimeNow;
+            TextBoxTimeSetup textBoxTimeSetup = new TextBoxTimeSetup();
+
+            TextBoxTimeSetup.TextBoxValue[] timeValues = null;
+            timeValues = textBoxTimeSetup.SelectSuitableTime(isTimeSizeBig, timeSpan);
+            TextBoxTimeSetup.TimeTextBoxItem timeTextBoxItem = new TextBoxTimeSetup.TimeTextBoxItem(date, dateTimeNow, textBoxResult, timeValues);
+        }
+
+        private void SendError(MaskedTextBox textBox, string errorMessage)
+        {
+            MessageBox.Show(errorMessage, errorName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            textBox.Clear();
+        }
+    }
+}
---
+++ WinFormsTaskCalculatingTheDate/GLOBAL_REFACTORING/TextBoxTimeSetup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsTaskCalculatingTheDate
+{
+    public class TextBoxTimeSetup
+    {
+        public enum TimeType
+        {
+            Default,
+            Seconds,
+            Minutes,
+            Hours,
+            Days,
+            Month,
+            Years
+        }
+
+        [Serializable] public class TextBoxValue
+        {
+            public TimeType timeType;
+            public string timeRusName;
+            public string timeSpan;
+
+            public TextBoxValue(TimeType enteredTimeType, string enteredTimeRusName, string enteredTimeSpan)
+            {
+                timeType = enteredTimeType;
+                timeRusName = enteredTimeRusName;
+                timeSpan = enteredTimeSpan;
+            }
+
+        }
+
+        public class TimeTextBoxItem
+        {
+            public TimeTextBoxItem(DateTime date, DateTime dateTimeNow, TextBox textBoxResult, TextBoxValue[] textBoxValue)
+            {
+                SetTimeInTextBox(textBoxResult, textBoxValue);
+            }
+
+            private void SetTimeInTextBox(TextBox textBoxResult, TextBoxValue[] textBoxValue)
+            {
+                int linesInTextBoxTime = 3;
+                for (int i = 0; i < linesInTextBoxTime; i++)
+                {
+                    textBoxResult.Text += textBoxValue[i].timeRusName + textBoxValue[i].timeSpan;
+                    SkipLine(textBoxResult);
+                }
+            }
+            private void SkipLine(TextBox textBoxForSkip)
+            {
+                textBoxForSkip.Text += "\r\n";
+            }
+        }
+
+        private TextBoxValue[] InitializeBigTime(TimeSpan enteredTimeSpan)
+        {
+            TextBoxValue[] textBoxValues = new TextBoxValue[3];
+            textBoxValues[0] = new TextBoxValue(TimeType.Days, "Дней: ", ((int)enteredTimeSpan.TotalDays + 1).ToString());
+            textBoxValues[1] = new TextBoxValue(TimeType.Month, "Месяцев: ", ((int)enteredTimeSpan.TotalDays / 30).ToString());
+            textBoxValues[2] = new TextBoxValue(TimeType.Years, "Годов: ", ((int)enteredTimeSpan.TotalDays / 365).ToString());
+            return textBoxValues;
+        }
+
+        private TextBoxValue[] InitializeSmallTime(TimeSpan enteredTimeSpan)
+        {
+            TextBoxValue[] textBoxValues = new TextBoxValue[3];
+            textBoxValues[0] = new TextBoxValue(TimeType.Hours, "Секунд: ", ((int)enteredTimeSpan.TotalSeconds).ToString());
+            textBoxValues[1] = new TextBoxValue(TimeType.Minutes, "Минут: ", ((int)enteredTimeSpan.TotalMinutes).ToString());
+            textBoxValues[2] = new TextBoxValue(TimeType.Seconds, "Часов: ", ((int)enteredTimeSpan.TotalHours).ToString());
+            return textBoxValues;
+        }
+
+        public TextBoxValue[] SelectSuitableTime(bool isTimeSizeBig, TimeSpan timeSpan)
+        {
+            return isTimeSizeBig ? InitializeBigTime(timeSpan) : InitializeSmallTime(timeSpan);
+        }
+    }
+}
