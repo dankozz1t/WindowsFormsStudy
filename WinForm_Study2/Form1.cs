f3fc90bcf892237eab5db257c70b3a0069c2d955
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-10 10:45:07 +0200

    Testing create multiple form

---
+++ WinForm_Study2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_Study2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            //f.Product = textBox1.Text;
+            //f.Show();
+            //f.ShowDialog();
+            //f.ShowDialog(textBox1.Text);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            //f.Product = textBox1.Text;
+            //f.Show();
+            //f.ShowDialog();
+            //f.ShowDialog(textBox1.Text);
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Add(f.Product);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
---
+++ WinForm_Study2/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_Study2
+{
+    public partial class Form2 : Form
+    {
+
+
+        //Передача через свойство
+        //public string Product
+        //{
+        //    set
+        //    {
+        //        textBox1.Text = value;
+        //    }
+        //}
+
+        //Передача через параметр
+        //public Form2(string str)
+        //{
+        //    InitializeComponent();
+        //    textBox1.Text = str;
+        //}
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        public string Product
+        {
+            set { textBox1.Text = value; }
+            get { return textBox1.Text; }
+        }
+        //Передача через перегрузку 
+        public DialogResult ShowDialog(string s)
+        {
+            textBox1.Text = s;
+            return ShowDialog();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
