7f6653a5eff4a9c9c12c156487286d2c098b92a0
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-10 12:08:01 +0200

    SaveFileDialog / OpenFileDialog

--- WinForm_Study2/Form1.Designer.cs
+++ WinForm_Study2/Form1.Designer.cs
@@ -35,6 +35,17 @@ namespace WinForm_Study2
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonLoadF = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonSaveF = new System.Windows.Forms.Button();
+            this.buttonDelF = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.buttonFont = new System.Windows.Forms.Button();
+            this.buttonRandomColor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -92,11 +103,114 @@ namespace WinForm_Study2
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(306, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(428, 298);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            // 
+            // buttonLoadF
+            // 
+            this.buttonLoadF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.buttonLoadF.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonLoadF.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLoadF.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonLoadF.Location = new System.Drawing.Point(306, 371);
+            this.buttonLoadF.Name = "buttonLoadF";
+            this.buttonLoadF.Size = new System.Drawing.Size(105, 39);
+            this.buttonLoadF.TabIndex = 7;
+            this.buttonLoadF.Text = "Загрузить";
+            this.buttonLoadF.UseVisualStyleBackColor = false;
+            this.buttonLoadF.Click += new System.EventHandler(this.buttonLoadF_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonSaveF
+            // 
+            this.buttonSaveF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.buttonSaveF.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSaveF.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSaveF.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonSaveF.Location = new System.Drawing.Point(417, 371);
+            this.buttonSaveF.Name = "buttonSaveF";
+            this.buttonSaveF.Size = new System.Drawing.Size(105, 39);
+            this.buttonSaveF.TabIndex = 10;
+            this.buttonSaveF.Text = "Сохранить";
+            this.buttonSaveF.UseVisualStyleBackColor = false;
+            this.buttonSaveF.Click += new System.EventHandler(this.buttonSaveF_Click);
+            // 
+            // buttonDelF
+            // 
+            this.buttonDelF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.buttonDelF.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonDelF.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDelF.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonDelF.Location = new System.Drawing.Point(629, 371);
+            this.buttonDelF.Name = "buttonDelF";
+            this.buttonDelF.Size = new System.Drawing.Size(105, 39);
+            this.buttonDelF.TabIndex = 11;
+            this.buttonDelF.Text = "Удалить";
+            this.buttonDelF.UseVisualStyleBackColor = false;
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.buttonColor.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonColor.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonColor.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonColor.Location = new System.Drawing.Point(578, 34);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(75, 39);
+            this.buttonColor.TabIndex = 12;
+            this.buttonColor.Text = "Цвет";
+            this.buttonColor.UseVisualStyleBackColor = false;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // buttonFont
+            // 
+            this.buttonFont.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.buttonFont.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonFont.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFont.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonFont.Location = new System.Drawing.Point(659, 34);
+            this.buttonFont.Name = "buttonFont";
+            this.buttonFont.Size = new System.Drawing.Size(75, 39);
+            this.buttonFont.TabIndex = 13;
+            this.buttonFont.Text = "Шрифт";
+            this.buttonFont.UseVisualStyleBackColor = false;
+            this.buttonFont.Click += new System.EventHandler(this.buttonFont_Click);
+            // 
+            // buttonRandomColor
+            // 
+            this.buttonRandomColor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.buttonRandomColor.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonRandomColor.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRandomColor.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonRandomColor.Location = new System.Drawing.Point(730, 79);
+            this.buttonRandomColor.Name = "buttonRandomColor";
+            this.buttonRandomColor.Size = new System.Drawing.Size(70, 24);
+            this.buttonRandomColor.TabIndex = 14;
+            this.buttonRandomColor.Text = "Радуга";
+            this.buttonRandomColor.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(308, 584);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(130)))), ((int)(((byte)(162)))));
+            this.ClientSize = new System.Drawing.Size(804, 584);
+            this.Controls.Add(this.buttonRandomColor);
+            this.Controls.Add(this.buttonFont);
+            this.Controls.Add(this.buttonColor);
+            this.Controls.Add(this.buttonDelF);
+            this.Controls.Add(this.buttonSaveF);
+            this.Controls.Add(this.buttonLoadF);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -118,6 +232,17 @@ namespace WinForm_Study2
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonLoadF;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonSaveF;
+        private System.Windows.Forms.Button buttonDelF;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.Button buttonFont;
+        private System.Windows.Forms.Button buttonRandomColor;
     }
 }
 
--- WinForm_Study2/Form1.cs
+++ WinForm_Study2/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace WinForm_Study2
 {
     public partial class Form1 : Form
     {
+        private Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
@@ -49,6 +52,73 @@ namespace WinForm_Study2
         private void button4_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
+        }
+
+
+        private void buttonLoadF_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.Title = "Что-то открыть...";
+            open.Filter = "All files (*.*)|*.*|Text files (*.txt)|*.txt||";
+            open.FilterIndex = 2; //Начинает с второго фильтра
+
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader stream = File.OpenText(open.FileName);
+                richTextBox1.Text = stream.ReadToEnd();
+                stream.Close();
+            }
+
+        }
+
+        private void buttonSaveF_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Title = "Сохранить";
+
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter stream = new StreamWriter(save.FileName);
+                stream.Write(richTextBox1.Text);
+                stream.Close();
+            }
+        }
+
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = richTextBox1.SelectionColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = colorDialog1.Color;
+            }
+
+        }
+
+        private void buttonFont_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowColor = true;
+            fontDialog1.Font = richTextBox1.SelectionFont;
+            fontDialog1.Color = richTextBox1.SelectionColor;
+
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+                richTextBox1.SelectionColor = fontDialog1.Color;
+            }
+        }
+
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //string txt = richTextBox1.Text;
+            //richTextBox1.Text = "";
+            //for (int i = 0; i < txt.Length; i++)
+            //{
+            //    richTextBox1.SelectionColor = Color.FromArgb(random.Next(1, 250), random.Next(1, 250), random.Next(1, 250));
+            //    //richTextBox1.Text += txt[i];
+            //}
         }
     }
 }
