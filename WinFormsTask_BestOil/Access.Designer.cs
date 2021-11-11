655568a1a12bab17ab4edeffc6ec7f90840c2cc0
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-11 19:52:07 +0200

    authorization

---
+++ WinFormsTask_BestOil/Access.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace WinFormsTask_BestOil
+{
+    partial class Access
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Access));
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxpPassword = new System.Windows.Forms.TextBox();
+            this.buttonEntry = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.BackColor = System.Drawing.Color.LightGray;
+            this.textBoxLogin.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLogin.Location = new System.Drawing.Point(46, 45);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBoxLogin.Size = new System.Drawing.Size(194, 23);
+            this.textBoxLogin.TabIndex = 1;
+            this.textBoxLogin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxpPassword
+            // 
+            this.textBoxpPassword.BackColor = System.Drawing.Color.LightGray;
+            this.textBoxpPassword.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxpPassword.Location = new System.Drawing.Point(46, 84);
+            this.textBoxpPassword.Name = "textBoxpPassword";
+            this.textBoxpPassword.PasswordChar = '*';
+            this.textBoxpPassword.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBoxpPassword.Size = new System.Drawing.Size(194, 23);
+            this.textBoxpPassword.TabIndex = 2;
+            this.textBoxpPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonEntry
+            // 
+            this.buttonEntry.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(152)))), ((int)(((byte)(169)))));
+            this.buttonEntry.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonEntry.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEntry.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(22)))), ((int)(((byte)(11)))));
+            this.buttonEntry.Location = new System.Drawing.Point(46, 130);
+            this.buttonEntry.Name = "buttonEntry";
+            this.buttonEntry.Size = new System.Drawing.Size(75, 23);
+            this.buttonEntry.TabIndex = 27;
+            this.buttonEntry.Text = "Вход";
+            this.buttonEntry.UseVisualStyleBackColor = false;
+            this.buttonEntry.Click += new System.EventHandler(this.buttonEntry_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(152)))), ((int)(((byte)(169)))));
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonExit.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(22)))), ((int)(((byte)(11)))));
+            this.buttonExit.Location = new System.Drawing.Point(165, 130);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 28;
+            this.buttonExit.Text = "Выход";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 26);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 29;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 77);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(28, 27);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 30;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Access
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(47)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(294, 174);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonEntry);
+            this.Controls.Add(this.textBoxpPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Name = "Access";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Access";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxpPassword;
+        private System.Windows.Forms.Button buttonEntry;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ WinFormsTask_BestOil/Access.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsTask_BestOil
+{
+    public partial class Access : Form
+    {
+
+        public Access()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEntry_Click(object sender, EventArgs e)
+        {
+            if (textBoxLogin.Text == "admin" && textBoxpPassword.Text == "admin")
+            {
+                this.Hide();
+                BestOil bestOilAdmin = new BestOil(true);
+                bestOilAdmin.Text = "BestOil <<ADMIN>>";
+                bestOilAdmin.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                this.Hide();
+                BestOil bestOilAdmin = new BestOil(false);
+                bestOilAdmin.Text = "BestOil <<USER>>";
+                bestOilAdmin.ShowDialog();
+                this.Close();
+            }
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
